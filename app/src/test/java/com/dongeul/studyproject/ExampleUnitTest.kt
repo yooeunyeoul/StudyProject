642a6349302e8349cdf1e46391c52748980effc4
youeuneyol <ey.yoo@fanmaum.com>
2023-04-05 15:20:09 +0900

    Flow Study Test Code

--- app/src/test/java/com/dongeul/studyproject/ExampleUnitTest.kt
+++ app/src/test/java/com/dongeul/studyproject/ExampleUnitTest.kt
@@ -1,10 +1,12 @@
 package com.dongeul.studyproject
 
 import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.*
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 
@@ -14,12 +16,21 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @OptIn(ExperimentalCoroutinesApi::class)
-class ExampleUnitTest {
+
+//Given-When-Then
+class FLowTest {
 
     lateinit var valueFromFlow: Flow<Int>
 
+    private val testDispatcher = StandardTestDispatcher()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
     @Test
-    fun addition_isCorrect() {
+    fun conflate_test() {
         valueFromFlow = flow<Int> {
             for (i in 1..30) {
                 delay(100)
@@ -27,9 +38,70 @@ class ExampleUnitTest {
             }
         }
         runTest {
-            val result = valueFromFlow.conflate().onEach { delay(500) }.toList()
-            assertThat(listOf(1, 10, 20, 30)).isEqualTo(result)
+            valueFromFlow = valueFromFlow
+                .onEach { println("emit:$it") }
+                .conflate().buffer(onBufferOverflow = BufferOverflow.DROP_LATEST)
+//            assertThat(listOf(1, 10, 20, 30)).isEqualTo(result)
+            val collectJob = launch {
+                valueFromFlow.collect {
+                    delay(4000)
+                    println("consume$it")
+                }
+            }
+            advanceUntilIdle()
+
+
+            collectJob.cancel()
 
         }
+
+    }
+
+    @Test
+    fun flowTest_cold() {
+        runTest {
+            val myFlow = flow<Int> {
+                repeat(10) {
+                    println(it)
+                    emit(it)
+                    delay(1000)
+                }
+            }
+            advanceUntilIdle()
+            myFlow.collect {
+                println("#1st -> ${it}")
+            }
+
+
+            delay(2000)
+            myFlow.collect {
+                println("#2nd -> ${it}")
+            }
+        }
+    }
+
+    @Test
+    fun stateflowTest_hot() {
+        runTest {
+//            val myFlow = flow<Int> {
+//                repeat(10){
+//                    println(it)
+//                    emit(it)
+//                    delay(1000)
+//                }
+//            }
+//
+//            val myStateFlow = myFlow.stateIn(
+//                scope = testDispatcher,
+//                ,
+//                initialValue = 1
+//            )
+        }
+    }
+
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 }
